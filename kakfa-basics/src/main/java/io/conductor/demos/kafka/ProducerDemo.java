4087681104a375bdac566b5270e16fbf31dc877d
Ypaspistis Giannis <giannisypaspistis@gmail.com>
2024-10-05 11:25:20 +0300

    first commit

---
+++ kakfa-basics/src/main/java/io/conductor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,47 @@
+package io.conductor.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+
+        log.info("Starting kafka producer demo");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer Record
+        ProducerRecord<String, String> producerRecord =
+                // demo_java is the topic and hello world is the value
+                new ProducerRecord<>("demo_java" , "hello world");
+
+        // send the data
+        producer.send(producerRecord);
+
+        // tell the producer to send all the data and block until done -- synchronous
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+
+    }
+}
+
+
